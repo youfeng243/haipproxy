7cd2d552e7021c4ee9dc4ce2ed50685fae518f4a
youfeng <youfeng@stargraph.cn>
2018-07-23 20:26:41 +0800

    修正部署脚本

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 set -ex
 
-pip3 install --user virtualenv==16.0.0 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+pip install --user virtualenv==16.0.0 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
 home=`pwd`
 rm -rf venv
 echo "deploy path ${home}"
-virtualenv ${home}/venv --python=python3 --no-setuptools
+virtualenv ${home}/venv --python=python3.6 --no-setuptools
 ${home}/venv/bin/pip install Cython==0.28.3 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
 ${home}/venv/bin/pip install setuptools==39.1.0 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
 ${home}/venv/bin/pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
