ad97aeedfccd655e668e3cfdc3098c80e84b3455
youfeng <youfeng@stargraph.cn>
2018-07-23 16:56:24 +0800

    修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ home=`pwd`
 rm -rf venv
 echo "deploy path ${home}"
 virtualenv ${home}/venv --python=python3 --no-setuptools
-${home}/venv/bin/pip3 install Cython==0.28.3 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
-${home}/venv/bin/pip3 install setuptools==39.1.0 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
-${home}/venv/bin/pip3 install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+${home}/venv/bin/pip install Cython==0.28.3 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+${home}/venv/bin/pip install setuptools==39.1.0 -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
+${home}/venv/bin/pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
 echo "deploy finish"
